1691f50ac522c75270554a32be799a52a94f6c0e
Chaehy <chlee7670@cau.ac.kr>
2023-04-19 16:39:29 -0700

    add: initial setting

---
+++ chloe_frontend/tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: ["./src/**/*.{html,js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      primary: "#243c5a",
+      secondary: {
+        100: "#243c5a",
+      },
+    },
+  },
+  plugins: [],
+};
