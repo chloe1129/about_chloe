cc246905c48df3e0ff312a63537adb26256e9792
Chaehy <chlee7670@cau.ac.kr>
2023-04-19 22:16:31 -0700

    add: header menu reactive

--- chloe_frontend/src/components/Navigation.tsx
+++ chloe_frontend/src/components/Navigation.tsx
@@ -6,9 +6,11 @@ import {
   useParams,
   NavLink,
 } from "react-router-dom";
+import classNames from "classnames";
 
 function Navigation() {
   const [mouseOn, setMouseOn] = useState("");
+  const [menuToggle, setMenuToggle] = useState(false);
 
   const handleHoverEffectOn = (e: any) => {
     setMouseOn(e.target.innerText);
@@ -19,14 +21,15 @@ function Navigation() {
 
   return (
     <div
-      className="sticky z-999 w-screen flex place-items-center justify-center top-0 mb-3"
+      className="sticky z-999 w-screen md:flex top-0 mb-3 sm:space-y-4"
       style={{ backgroundColor: "#E6E6FA" }}
     >
       <nav className="flex items-center justify-between space-x-10 mx-36">
-        <div className="justify-start shrink-0 pl-8 pr-36 py-8 text-3xl font-bold text-violet-800">
+        <div className="justify-start shrink-0 pl-2 pr-36 py-8 text-3xl font-bold text-violet-800">
           <Link to={"/"}>CHLOE Lee</Link>
         </div>
-        <div className="flex justify-end p-2">
+
+        <div className={"hidden md:flex justify-end p-2"}>
           <div
             onMouseEnter={handleHoverEffectOn}
             onMouseLeave={handleHoverEffectOff}
@@ -109,6 +112,80 @@ function Navigation() {
           </div>
         </div>
       </nav>
+      <div className="md:hidden absolute right-5 top-5">
+        <button onClick={() => setMenuToggle(!menuToggle)}>
+          {menuToggle ? (
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-6 w-6"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth={2}
+                d="M6 18L18 6M6 6l12 12"
+              />
+            </svg>
+          ) : (
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-6 w-6"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth={2}
+                d="M4 6h16M4 12h16M4 18h16"
+              />
+            </svg>
+          )}
+        </button>
+      </div>
+      <div
+        className={classNames(
+          "md:hidden shrink-0 flex-col space-y-2 place-items-center items-center justify-center",
+          {
+            hidden: !menuToggle,
+          }
+        )}
+      >
+        <NavLink
+          to="/"
+          className="block py-2 px-4 text-sm text-gray-700 hover:text-violet-800"
+        >
+          About Chloe
+        </NavLink>
+        <NavLink
+          to="/experience"
+          className="block py-2 px-4 text-sm text-gray-700 hover:text-violet-800"
+        >
+          Experience
+        </NavLink>
+        <NavLink
+          to="/education"
+          className="block py-2 px-4 text-sm text-gray-700 hover:text-violet-800"
+        >
+          Education
+        </NavLink>
+        <NavLink
+          to="/projects"
+          className="block py-2 px-4 text-sm text-gray-700 hover:text-violet-800"
+        >
+          Projects
+        </NavLink>
+        <NavLink
+          to="/contact"
+          className="block py-2 px-4 text-sm text-gray-700 hover:text-violet-800"
+        >
+          Contact
+        </NavLink>
+      </div>
     </div>
   );
 }
