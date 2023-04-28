80130853f94c0106e5547e3e6e8b2f813be746fd
Chaehy <chlee7670@cau.ac.kr>
2023-04-27 22:30:31 -0700

    style: experience box

---
+++ chloe_frontend/src/components/ExperienceBox.tsx
@@ -0,0 +1,19 @@
+export default function ExperienceBox() {
+  return (
+    <div className="flex flex-col justify-start mx-3 my-5 font-sans">
+      <div className="flex flex-row space-x-3 items-center text-slate-700 p-4">
+        <div className="text-xl font-semibold">AI/SW Intern</div>
+        <div className="text-sm">Uniquify</div>
+      </div>
+      <div className="flex space-x-3 text-sm px-4 py-2">
+        <div>Sep 2022</div>
+        <div>-</div>
+        <div>Present</div>
+      </div>
+      <div className="flex p-4">
+        I am currently working as a AI/SW Intern at Uniquify in Santa Clara, CA.
+        I am currently working as a AI/SW Intern at Uniquify in Santa Clara, CA.
+      </div>
+    </div>
+  );
+}
