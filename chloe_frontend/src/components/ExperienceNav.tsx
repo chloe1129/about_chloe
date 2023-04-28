01011a97a32664839d6a1347ff23d40eb4c4d1e1
Chaehy <chlee7670@cau.ac.kr>
2023-04-27 22:30:13 -0700

    style: experience nav bar

---
+++ chloe_frontend/src/components/ExperienceNav.tsx
@@ -0,0 +1,20 @@
+export default function BoxNav() {
+  return (
+    <div className="">
+      <nav className="flex justify-start space-x-8 px-4 font-sans">
+        <button className="font-medium px-4 py-2 text-slate-700 hover:text-violet-800">
+          Uniquify
+        </button>
+        <button className="font-medium px-4 py-2 text-slate-700 hover:text-violet-800">
+          Startus
+        </button>
+        <button className="font-medium px-4 py-2 text-slate-700 hover:text-violet-800">
+          HandU
+        </button>
+        <button className="font-medium px-4 py-2 text-slate-700 hover:text-violet-800">
+          CVML
+        </button>
+      </nav>
+    </div>
+  );
+}
