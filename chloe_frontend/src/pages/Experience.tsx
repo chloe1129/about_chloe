5df4bbb5567e3c20db49ee180c21abcaa2ed678f
Chaehy <chlee7670@cau.ac.kr>
2023-04-28 23:36:56 -0700

    style: add experience page

--- chloe_frontend/src/pages/Experience.tsx
+++ chloe_frontend/src/pages/Experience.tsx
@@ -1,7 +1,22 @@
 import React from "react";
+import ExperienceBox from "../components/ExperienceBox";
+import ExperienceNav from "../components/ExperienceNav";
 
 function Experience() {
-  return <div className="p-50 text-lg">Experience</div>;
+  return (
+    <div
+      className="flex justify-center pb-20 h-screen"
+      style={{ backgroundColor: "#E6E6FA" }}
+    >
+      <div className="flex flex-col mx-10 my-10 align-middle items-center">
+        <div className="text-3xl font-semibold text-violet-800">Experience</div>
+        <div className="my-8 rounded-xl bg-white py-4 px-8">
+          <ExperienceNav />
+          <ExperienceBox />
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Experience;
